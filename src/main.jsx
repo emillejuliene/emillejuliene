c54926b427256bc59107d2a2cb0188e2ddb44af3
Emille Juliene <73906512+emillejuliene@users.noreply.github.com>
2026-04-21 10:30:19 +0800

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./My_Portfolio.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
